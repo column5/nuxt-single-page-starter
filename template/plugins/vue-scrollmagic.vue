deb4043131ebd1822cabe3d4ba2a250b7d823a91
Cale Dunlap <cdunlap@columnfivemedia.com>
2018-08-29 14:40:47 -0700

    Added some verbiage about the usage of the ks-vue-scrollmagic Vue plugin

--- template/plugins/vue-scrollmagic.vue
+++ template/plugins/vue-scrollmagic.vue
@@ -1,3 +1,10 @@
 import Vue from 'vue';
 import KsVueScrollmagic from 'ks-vue-scrollmagic'
 Vue.use(KsVueScrollmagic)
+
+/*
+Inside of a Vue component, this plugin exposes `this.$gsap` and `this.$scrollmagic`
+with their respective libraries. No need to use them globally.
+
+For more information see https://www.npmjs.com/package/ks-vue-scrollmagic
+*/
